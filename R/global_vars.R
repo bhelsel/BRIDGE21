10d17e89b55af77fa54c51d7b1617d0f5460030e
Brian Helsel <bhelsel@kumc.edu>
2025-06-16 21:21:28 -0500

    Update global_vars.R

--- R/global_vars.R
+++ R/global_vars.R
@@ -1,5 +1,11 @@
 # Copyright © 2022 University of Kansas. All rights reserved.
 
-if(getRversion() >= "2.15.1")  utils::globalVariables(c(
-  "ptid", "ENMO", "category", "timestamp"
-))
+if (getRversion() >= "2.15.1") {
+  utils::globalVariables(c(
+    "ptid",
+    "ENMO",
+    "category",
+    "timestamp",
+    "label"
+  ))
+}
